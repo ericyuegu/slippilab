53219ba8a9afa134af7809802ee4b712279f220e
Graham Preston <graham.preston@gmail.com>
2026-02-01 17:17:36 +0100

    Install and configure wasm build of tm_replay

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import wasmPlugin from "vite-plugin-wasm";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   assetsInclude: [/.*zip$/, /.*ttf$/],
-  plugins: [solidPlugin(), viteTsconfigPaths()],
+  plugins: [solidPlugin(), wasmPlugin(), viteTsconfigPaths()],
   resolve: {
     conditions: ["browser"],
   },
